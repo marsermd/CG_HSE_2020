1bd7a078b807384e303df849c1925939505e1342
vasyoid <vasyoid@gmail.com>
2020-09-19 14:22:30 +0300

    Implement marching cubes on CPU

--- Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
+++ Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
+    private const float Step = 0.1f;
+    private const float Eps = 0.001f;
     public MetaBallField Field = new MetaBallField();
     
     private MeshFilter _filter;
@@ -26,37 +29,12 @@ public class MeshGenerator : MonoBehaviour
         
         // Just a little optimization, telling unity that the mesh is going to be updated frequently
         _mesh.MarkDynamic();
+        
+        GenerateMesh();
     }
 
-    /// <summary>
-    /// Executed by Unity on every frame <see cref="https://docs.unity3d.com/Manual/ExecutionOrder.html"/>
-    /// You can use it to animate something in runtime.
-    /// </summary>
-    private void Update()
+    private void GenerateMesh()
     {
-        List<Vector3> cubeVertices = new List<Vector3>
-        {
-            new Vector3(0, 0, 0), // 0
-            new Vector3(0, 1, 0), // 1
-            new Vector3(1, 1, 0), // 2
-            new Vector3(1, 0, 0), // 3
-            new Vector3(0, 0, 1), // 4
-            new Vector3(0, 1, 1), // 5
-            new Vector3(1, 1, 1), // 6
-            new Vector3(1, 0, 1), // 7
-        };
-
-        int[] sourceTriangles =
-        {
-            0, 1, 2, 2, 3, 0, // front
-            3, 2, 6, 6, 7, 3, // right
-            7, 6, 5, 5, 4, 7, // back
-            0, 4, 5, 5, 1, 0, // left
-            0, 3, 7, 7, 4, 0, // bottom
-            1, 5, 6, 6, 2, 1, // top
-        };
-
-        
         vertices.Clear();
         indices.Clear();
         normals.Clear();
@@ -66,29 +44,85 @@ public class MeshGenerator : MonoBehaviour
         // Generate mesh here. Below is a sample code of a cube generation.
         // ----------------------------------------------------------------
 
-        // What is going to happen if we don't split the vertices? Check it out by yourself by passing
-        // sourceVertices and sourceTriangles to the mesh.
-        for (int i = 0; i < sourceTriangles.Length; i++)
+        for (float x = -4; x < 4; x += Step)
         {
-            indices.Add(vertices.Count);
-            Vector3 vertexPos = cubeVertices[sourceTriangles[i]];
-            
-            //Uncomment for some animation:
-            //vertexPos += new Vector3
-            //(
-            //    Mathf.Sin(Time.time + vertexPos.z),
-            //    Mathf.Sin(Time.time + vertexPos.y),
-            //    Mathf.Sin(Time.time + vertexPos.x)
-            //);
-            
-            vertices.Add(vertexPos);
+            for (float y = -4; y < 4; y += Step)
+            {
+                for (float z = -4; z < 4; z += Step)
+                {
+                    int mask = 0;
+                    for (int i = 0; i < 8; ++i)
+                    {
+                        Vector3 vertex = MarchingCubes.Tables._cubeVertices[i];
+                        Vector3 pos1 = new Vector3
+                        (
+                            x + vertex.x * Step,
+                            y + vertex.y * Step,
+                            z + vertex.z * Step
+                        );
+                        float f = Field.F(pos1);
+                        mask |= (f > 0 ? 1 : 0) << i;
+                    }
+                    byte trianglesCount = MarchingCubes.Tables.CaseToTrianglesCount[mask];
+                    for (int i = 0; i < trianglesCount; ++i)
+                    {
+                        int3 triangleEdges = MarchingCubes.Tables.CaseToVertices[mask][i];
+                        for (int e = 0; e < 3; ++e)
+                        {
+                            int[] edge = MarchingCubes.Tables._cubeEdges[triangleEdges[e]];
+                            Vector3 vertex1 = MarchingCubes.Tables._cubeVertices[edge[0]];
+                            Vector3 vertex2 = MarchingCubes.Tables._cubeVertices[edge[1]];
+                            Vector3 a = new Vector3
+                            (
+                                x + vertex1.x * Step,
+                                y + vertex1.y * Step,
+                                z + vertex1.z * Step
+                            );
+                            Vector3 b = new Vector3
+                            (
+                                x + vertex2.x * Step,
+                                y + vertex2.y * Step,
+                                z + vertex2.z * Step
+                            );
+                            float fa = Field.F(a);
+                            float fb = Field.F(b);
+                            float t = -fb / (fa - fb);
+                            Vector3 v = new Vector3
+                            (
+                                a.x * t + b.x * (1 - t),
+                                a.y * t + b.y * (1 - t),
+                                a.z * t + b.z * (1 - t)
+                            );
+                            Vector3 dx = Vector3.right * Eps;
+                            Vector3 dy = Vector3.up * Eps;
+                            Vector3 dz = Vector3.forward * Eps;
+                            Vector3 n = new Vector3
+                            (
+                                Field.F(v + dx) - Field.F(v - dx),
+                                Field.F(v + dy) - Field.F(v - dy),
+                                Field.F(v + dz) - Field.F(v - dz)
+                            ).normalized;
+                            indices.Add(vertices.Count);
+                            vertices.Add(v);
+                            normals.Add(n);
+                        }
+                    }
+                }
+            }
         }
-
+    }
+    
+    /// <summary>
+    /// Executed by Unity on every frame <see cref="https://docs.unity3d.com/Manual/ExecutionOrder.html"/>
+    /// You can use it to animate something in runtime.
+    /// </summary>
+    private void Update()
+    {
         // Here unity automatically assumes that vertices are points and hence (x, y, z) will be represented as (x, y, z, 1) in homogenous coordinates
         _mesh.Clear();
         _mesh.SetVertices(vertices);
         _mesh.SetTriangles(indices, 0);
-        _mesh.RecalculateNormals(); // Use _mesh.SetNormals(normals) instead when you calculate them
+        _mesh.SetNormals(normals);
 
         // Upload mesh data to the GPU
         _mesh.UploadMeshData(false);
